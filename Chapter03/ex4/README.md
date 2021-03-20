f9af6ef6f3bb37dc380677c61ab477d22baac1b8
Daniel Frank <dfrank@luxurypresence.com>
2021-03-19 22:41:35 -0700

    Exercise 3.4

--- Chapter03/ex4/README.md
+++ Chapter03/ex4/README.md
@@ -2,7 +2,9 @@
 Modify function from exercise 3.3 so that it produces an error if the timestamp at the moment of a tick (including the initial one added to exercise 3.3) is divisible by 5. Propagate the error using both the callback and the event emitter.
 
 ## Notes
+Gets error which is generally handled. Exception is that similar to not seeing Tick 1, if the error occurs before the event-handlers are set-up, the program crashes.
 
+We should probably set this up as a class and set up the event-handlers before we call the processing. (Not doing that since that wasn't the exercise. Were this a real project, I would discuss with the team that we have this problem (Or just fix it if I have tha authority and it won't break things for other folks using this.))
 
 ## Run
 
--- Chapter03/ex4/index.js
+++ Chapter03/ex4/index.js
@@ -1,28 +1,48 @@
 import { EventEmitter } from 'events';
 
+function dateCheck() {
+  if (Date.now() % 5 === 0) {
+    throw new Error('Divisible by 5');
+  }
+}
+
 function tickEventStart(number, callback) {
   const emitter = new EventEmitter();
+  const handleError = err => {
+    emitter.emit('error', err);
+    callback(err);
+  };
   const EVENT_INTERVAL = 50;
   let tickCount = 1;
-  emitter.emit('tick', tickCount);
+  try {
+    dateCheck();
+    emitter.emit('tick', tickCount);
+  } catch(err) {
+    handleError(err);
+  }
   let millisecondsCount = 0;
   const tickEvent = () => {
-    tickCount += 1;
-    emitter.emit('tick', tickCount);
-    millisecondsCount += EVENT_INTERVAL;
-    if (millisecondsCount >= number) {
-      callback(null, tickCount);
-    } else {
-      setTimeout(tickEvent, EVENT_INTERVAL);
+    try {
+      dateCheck();
+      tickCount += 1;
+      emitter.emit('tick', tickCount);
+      millisecondsCount += EVENT_INTERVAL;
+      if (millisecondsCount >= number) {
+        callback(null, tickCount);
+      } else {
+        setTimeout(tickEvent, EVENT_INTERVAL);
+      }
+    } catch(err) {
+      handleError(err);
     }
   };
   setTimeout(tickEvent, EVENT_INTERVAL);
   return emitter
 }
 
-tickEventStart(735, (err, tickCount) => {
+tickEventStart(737, (err, tickCount) => {
   if (err) {
-    return console.error(err);
+    return console.error(`Callback error ${err.message}`);
   }
   console.log(`${tickCount} ticks were done`);
 })
