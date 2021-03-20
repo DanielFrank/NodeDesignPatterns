1fc4775314a4613085f215484f1691c2c6decba7
Daniel Frank <dfrank@luxurypresence.com>
2021-03-19 21:55:32 -0700

    Did exercise 3.1

--- Chapter03/ex1/index.js
+++ Chapter03/ex1/index.js
@@ -14,13 +14,14 @@ class FindRegex extends EventEmitter {
   }
 
   find () {
+    this.emit('findStart', this.files)
     for (const file of this.files) {
       readFile(file, 'utf8', (err, content) => {
         if (err) {
           return this.emit('error', err)
         }
 
-        this.emit('fileread', file)
+        this.emit('fileRead', file)
 
         const match = content.match(this.regex)
         if (match) {
@@ -36,6 +37,7 @@ const findRegexInstance = new FindRegex(/hello \w+/)
 findRegexInstance
   .addFile('fileA.txt')
   .addFile('fileB.json')
+  .on('findStart', files => console.log(`${files.length} files are being searched in find`))
   .find()
   .on('found', (file, match) => console.log(`Matched "${match}" in file ${file}`))
   .on('error', err => console.error(`Error emitted ${err.message}`))
